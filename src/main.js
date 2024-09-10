6dc8221702bfe2b24155ece0377bbb6c68de5797
Daniele Gugliuzza <gruppostudioarces@gmail.com>
2024-09-10 10:17:38 +0200

    .

--- src/main.js
+++ src/main.js
@@ -3,5 +3,10 @@ import './assets/main.css';
 import { createApp } from 'vue';
 import App from './App.vue';
 
+app.config.globalProperties.Window = window;
+app.config.globalProperties.Document = window.document;
+app.config.globalProperties.history = window.history;
+app.config.globalProperties.JQuery = window.$;
+
 const app = createApp(App);
 app.mount('#app');
