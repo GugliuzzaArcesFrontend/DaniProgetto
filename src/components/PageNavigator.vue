86dfb62a3d5c4cc14d3e3d36fe65df2771c2db1b
Daniele Gugliuzza <gruppostudioarces@gmail.com>
2024-09-16 17:46:47 +0200

    .

---
+++ src/components/PageNavigator.vue
@@ -0,0 +1,45 @@
+<template>
+    <ul v-show="totalPages > 1 && !isCollapsed" class="pagination pagination-sm">
+        <li v-if="totalPages > 1 && currentPage > 1" class="page-item">
+            <a @click="changePage(1)" class="page-link">««</a>
+        </li>
+        <!-- <li v-if="totalPages > 1 && currentPage > 1" class="page-item">
+                        <a @click="changePage(currentPage - 1)" class="page-link">«</a>
+                    </li> -->
+        <li v-for="page in totalPages" class="page-item">
+            <Void v-if="page > currentPage - 3 && page < currentPage + 3">
+                <a v-if="page != currentPage" @click="changePage(page)" class="page-link">{{ page }}</a>
+                <a v-else class="page-link">...</a>
+            </Void>
+        </li>
+        <!-- <li v-if="currentPage < totalPages" class="page-item">
+                        <a @click="changePage(currentPage + 1)" class="page-link">»</a>
+                    </li> -->
+        <li v-if="currentPage < totalPages" class="page-item">
+            <a @click="changePage(totalPages)" class="page-link">»»</a>
+        </li>
+    </ul>
+</template>
+<script>
+export default {
+    name: 'PageNavigator',
+    props: {
+        totalPages: {
+            type: Number,
+            required: true
+        },
+        currentPage: {
+            type: Number,
+            required: true
+        },
+        isCollapsed: {
+            type: Boolean,
+            required: true
+        },
+        changePage: {
+            type: Function,
+            required: true
+        }
+    }
+}
+</script>
